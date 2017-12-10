a127624b95b40a347e6c0727f8ca1dc6c218a32a
Elgoist <f.kahl@yahoo.de>
2017-12-10 20:01:23 +0100

    V2 - refactoring and rethinking Jobs

--- screeps.com/v2/main.control.js
+++ screeps.com/v2/main.control.js
@@ -31,49 +31,27 @@ let mainControl = {
     },
 
     manageCreeps: function () {
-
-        if (Game.spawns['Main'].room.energyAvailable < 300 && Game.spawns['Main'].room.energyCapacityAvailable === 300) {
-            Memory.jobs.feeding = 3 - Memory.stats.feeders
+        if (Game.spawns['Main'].room.energyAvailable < Game.spawns['Main'].room.energyCapacityAvailable) {
+            Memory.jobs.feeding = 5 * Game.spawns['Main'].room - Memory.stats.feeders
         } else if (Game.spawns['Main'].room.energyAvailable < 550 && Game.spawns['Main'].room.energyCapacityAvailable === 550) {
             Memory.jobs.feeding = 4 - Memory.stats.feeders
         }
 
 
-        let constructionSites = Game.spawns['Main'].room.find(FIND_MY_CONSTRUCTION_SITES).length;
-        if (constructionSites === 0)
-            Memory.jobs.building = 0
-        if (constructionSites > 1)
-            Memory.jobs.building = 3
-        if (constructionSites > 10)
-            Memory.jobs.building = 5
-        if (constructionSites >= 20)
-            Memory.jobs.building = 6
-        if (constructionSites >= 50)
-            Memory.jobs.building = 7
-        if (constructionSites >= 100)
-            Memory.jobs.building = 8
 
+
+
+
+        Memory.buildcounter+= 1
+        if(Memory.buildcounter === 5){
+          Memory.buildcounter=0
+          this.updateJobBuilders()
+        }
         Memory.repaircounter += 1
-        console.log(Memory.repaircounter)
-
         if (Memory.repaircounter === 50) {
             Memory.repaircounter = 0
-
-
-            let repairables = Game.spawns['Main'].room.find(FIND_STRUCTURES,
-                {
-                    filter: (s) => {
-                        return s.structureType !== STRUCTURE_WALL && s.hits <= s.hitsMax - 200;
-                    }
-                }).length;
-            console.log(repairables)
-            console.log(repairables)
-            console.log(repairables)
-            if (repairables > 10)
-                Memory.jobs.repairing++
-            if (repairables < 3)
-                Memory.jobs.repairing--
-        }
+            this.updateJobRepairers()
+          }
 
 
         for (let name in Game.creeps) {
@@ -93,6 +71,50 @@ let mainControl = {
                 }
             }
         }
+    },
+    updateJobRepairers: function(){
+      let repairables = Game.spawns['Main'].room.find(FIND_STRUCTURES,
+          {
+              filter: (s) => {
+                  return s.structureType !== STRUCTURE_WALL && s.hits <= s.hitsMax - 200;
+              }
+          }).length;
+
+      for(let i =0; i<repairables.length();i++){
+        requiredStructurePoints += repairables[i].hitsMax- repairables[i].hits;
+      }
+
+      console.log('Repairables: ' + repairables +' with '+ requiredStructurePoints + ' missing StructurePaints')
+      if (repairables > 10)
+          Memory.jobs.repairing++
+      if (repairables < 3 && Memoy.jobs.repairing>=2)
+          Memory.jobs.repairing--
+
+    },
+
+    updateJobBuilders: function(){
+      let constructionSites = Game.spawns['Main'].room.find(FIND_MY_CONSTRUCTION_SITES);
+      let neededWork =0
+      for(let i = 0; i<constructionSites.lenght();i++){
+        neededWork += constructionSites[i].progressTotal -constructionSites[i].progress
+      }
+
+      if (neededWork === 0)
+          Memory.jobs.building = 0
+      if (neededWork > 1)
+          Memory.jobs.building = 4
+      if (neededWork > 1000)
+          Memory.jobs.building = 6
+      if (neededWork >= 2000)
+          Memory.jobs.building = 8
+      if (neededWork >= 5000)
+          Memory.jobs.building = 10
+      if (neededWork >= 10000)
+          Memory.jobs.building = 12
+      if (neededWork >= 20000)
+          Memory.jobs.building = 14
+      if(neededWork>= 50000)
+          Memory.jobs.building = 16
     }
 }
 module.exports = mainControl
